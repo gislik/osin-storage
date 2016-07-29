a6adf3ff5bdce600a4df616838cafa4e43f9b8fb
Collins <collins@gate.cloud>
2016-07-29 15:40:26 +1200

    Change UserData to string

--- authorize.go
+++ authorize.go
@@ -4,14 +4,14 @@ import "time"
 
 // Authorize data model
 type Authorize struct {
-	ClientID    string      // Client information
-	Code        string      `gorm:"primary_key"` // Authorization code
-	ExpiresIn   int32       // Token expiration in seconds
-	Scope       string      // Requested scope
-	RedirectUri string      // Redirect Uri from request
-	State       string      // State data from request
-	CreatedAt   time.Time   // Date created
-	UserData    interface{} `gorm:"jsonp"` // Data to be passed to storage. Not used by the library.
+	ClientID    string    // Client information
+	Code        string    `gorm:"primary_key"` // Authorization code
+	ExpiresIn   int32     // Token expiration in seconds
+	Scope       string    // Requested scope
+	RedirectUri string    // Redirect Uri from request
+	State       string    // State data from request
+	CreatedAt   time.Time // Date created
+	UserData    string    // Data to be passed to storage. Not used by the library.
 }
 
 //Tablename of Authorize data model
